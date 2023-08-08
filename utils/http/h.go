d3a546e709bc601c557b6d0ca43658bae4cd852e
peterouob <hellolinpeter@gmail.com>
2023-08-09 06:33:00 +0800

    新增跨域

--- utils/http/h.go
+++ utils/http/h.go
@@ -4,6 +4,7 @@ import (
 	"PSHOP/model/dao/redis"
 	"PSHOP/utils"
 	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strconv"
@@ -47,4 +48,19 @@ func FectApi(authD *utils.AccessDetails) (uint64, error) {
 	}
 	userId, _ := strconv.ParseUint(userid, 10, 64)
 	return userId, nil
+}
+
+func Cors(c *gin.Context) {
+	method := c.Request.Method
+	c.Header("Access-Control-Allow-Origin", c.GetHeader("Origin"))
+	fmt.Println(c.GetHeader("Origin"))
+	c.Header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+	c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Token")
+	c.Header("Access-Control-Expose-Headers", "Access-Control-Allow-Headers, Token")
+	c.Header("Access-Control-Allow-Credentials", "true")
+	if method == "OPTIONS" {
+		c.AbortWithStatus(http.StatusNoContent)
+		return
+	}
+	c.Next()
 }
